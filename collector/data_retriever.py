600de898829e06a465831fbf629856a740964e71
Max Sun <sunwmax@gmail.com>
2021-07-12 15:45:54 +0800

    Add data_retriever
    data_retriever writes MySQL table data to csv files.

---
+++ collector/data_retriever.py
@@ -0,0 +1,61 @@
+import mysql.connector
+from mysql.connector import Error
+import csv
+
+def create_connection(hostname: str, username: str, 
+                      password: str, database_name: str = None):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host = hostname,
+            user = username,
+            passwd = password,
+            database = database_name
+        )
+        print("Connection successful")
+        return connection
+    except Error as e:
+        print(f"Error: {e}")
+
+def fetch_results(query: str):
+    cursor = conn.cursor()
+    try:
+        cursor.execute(query)
+        return cursor.fetchall()
+    except Error as e:
+        return f"Error: {e}"
+
+def fetch_tables():
+    tables = [result[0] for result in fetch_results("SHOW TABLES;")]
+    return tables
+
+def fetch_columns(table_name: str):
+    columns = [result[0] for result in fetch_results(f"DESCRIBE {table_name};")]
+    return columns
+
+def fetch_table_data(table_name: str):
+    data = fetch_results(f"SELECT * FROM {table_name};")
+    return data
+
+def table_to_csv(table_name: str):
+    with open(f'{table_name}.csv', 'w', newline = '', encoding = 'utf-8') as csvfile:
+        column_names = fetch_columns(table_name)
+        table_data = fetch_table_data(table_name)      
+        writer = csv.writer(csvfile)
+        writer.writerow(column_names)
+        writer.writerows(table_data)
+
+def export_data_from_db():
+    tables = fetch_tables()
+    for table in tables:
+        table_to_csv(table)
+
+if __name__ == "__main__":
+    # Fill in your MySQL credentials
+    conn = create_connection(
+        hostname="",
+        username="",
+        password="",
+        database_name=""
+    )
+    export_data_from_db()
