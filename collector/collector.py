152c91b4a33a9e4c47634f1502a9018c91a0a6bd
Max Sun <sunwmax@gmail.com>
2021-07-12 16:09:31 +0800

    Added PRAW error handling

--- collector/collector.py
+++ collector/collector.py
@@ -1,22 +1,20 @@
 from scraper import MemeStatsScraper
 from database import DatabaseHelper
 from config import USER_PARAMS
-import threading
 import logging
 import schedule
-import time
-import datetime
+from datetime import datetime, timedelta
 
 # logging related info
 logging.basicConfig(
     filename = "worklog.log",
     filemode = "w",
     level = logging.INFO,
-    format = '%(asctime)s - %(name)s - %(message)s'
+    format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
 
 # Used for praw debugging
-for logger_name in ("praw", "prawcore"):
+for logger_name in ("scraper", "praw", "prawcore"):
     logger = logging.getLogger(logger_name)
     logger.setLevel(logging.DEBUG)
 
@@ -37,6 +35,10 @@ class DataCollector:
         self.dbhelper.connect_server()
         self.current_hot_ids = []
         self.current_new_ids = []
+        self.update_current_ids = dict()
+        for i in range(60):
+            minute_string = f":0{i}" if i < 10 else f":{i}"
+            self.update_current_ids[minute_string] = []
 
     # Prepares the database and contained tables for data insertion
     # Note: this function does not cover the case where database exists but the tables doesn't
@@ -49,9 +51,6 @@ class DataCollector:
             print("Found existing database. Connecting...")
             self.dbhelper.connect_database(database_name)
         logging.info(f"Connection to {database_name} has been establised. Data will be stored there.")
-
-    def __add_new_collection_task(self, meme_id: str, time: str):
-        schedule.every().hour.at(time).do(self.collect_existing_meme_data, meme_id)
 
     def __retrieve_valid_newest_id(self):
         for meme_id in self.current_new_ids:
@@ -76,7 +75,7 @@ class DataCollector:
             self.dbhelper.insert_meme_score(meme["id"], 0, 0)
             self.dbhelper.insert_meme_comments(meme["id"], 0, 0)
             self.dbhelper.insert_meme_status(meme["id"], 0, False)
-            self.__add_new_collection_task(meme["id"], meme["time_created"][-5:])
+            self.update_current_ids[meme["time_created"][-6:-3]].append(meme["id"])
             logging.info(f"{meme['id']} has been added to database")
         
         logging.info(f"{len(new_memes)} new memes added in total")
@@ -87,53 +86,106 @@ class DataCollector:
     # Retrieve current hottest "num_meme" meme ids
     def collect_current_hot_meme_ids(self, num_memes: int = 100):
         logging.info("Collecting current hot meme ids...")
-        self.current_hot_ids = [meme["id"] for meme in self.scraper.find_hot(num_memes)]
-        logging.info("Collected current hot meme ids")
-        self.__log_current_rate_limit()
-
-    # Updates a current meme submission
-    # Updates score, comments
-    # Check if post has entered hot
-    def collect_existing_meme_data(self, meme_id: str):
-        # Get the most recent entry from meme_status, and retrieve hours and is_hot
-        try:
-            hours_elapsed, is_hot = self.dbhelper.search_meme_latest_status(meme_id)
-        except ValueError:
-            logging.error(f"{meme_id} not found in meme_status")
-            return
+        current_hot_memes = self.scraper.find_hot(num_memes)
+        if current_hot_memes:
+            self.current_hot_ids = [meme["id"] for meme in current_hot_memes]
+            logging.info("Collected current hot meme ids")
         
-        hours_elapsed += 1
-        # Returns newest status
-        meme = self.scraper.find_specific(meme_id)
-        entered_hot = self.__is_hot(meme_id)
-        self.dbhelper.insert_meme_score(meme_id, hours_elapsed, meme["score"])
-        self.dbhelper.insert_meme_comments(meme_id, hours_elapsed, meme["num_comments"])
-        self.dbhelper.insert_meme_status(meme_id, hours_elapsed, entered_hot)
-        logging.info(f"{meme_id} is updated")
         self.__log_current_rate_limit()
 
-        # Update if meme has newly entered hot
-        if (not is_hot) and (entered_hot):
-            self.dbhelper.update_meme_info(meme_id, entered_hot)
-            logging.info(f"{meme_id} has entered hot, updated info")
-    
     def __is_hot(self, meme_id: str):
-        if meme_id in self.current_hot_ids:
-            return True
-        return False
+        return meme_id in self.current_hot_ids
 
-    def __scheduler(self):
-        schedule.every(5).minutes.do(self.collect_new_meme_data)
-        schedule.every(5).minutes.do(self.collect_current_hot_meme_ids)
+    # Updates multiple current meme submissions
+    # Will replace collect_existing_meme_data
+    def collect_existing_memes_data(self, time: str, update_hours: int = 24, failsafe: bool = False):
+        meme_ids = self.update_current_ids[time]
+        logging.info("Updating existing meme ids...")
+        if not meme_ids:
+            logging.info("No memes needed to be updated.")
+            return
 
+        updated_memes = self.scraper.find_multi_specific(meme_ids)
+
+        # Failsafe for updating existing memes:
+        # If memes cannot be updated (mainly due to connection error),
+        # These memes will be removed from the update list.
+        if failsafe and (updated_memes == None):
+            self.update_current_ids[time] = []
+            logging.info(f"Memes at {time} cannot be updated. Failsafe activated, these memes will not be updated anymore.")
+            return
+
+        for meme in updated_memes:
+            hours_elapsed, is_hot = self.dbhelper.search_meme_latest_status(meme["id"])
+            hours_elapsed += 1
+            entered_hot = self.__is_hot(meme["id"])
+            self.dbhelper.insert_meme_score(meme["id"], hours_elapsed, meme["score"])
+            self.dbhelper.insert_meme_comments(meme["id"], hours_elapsed, meme["num_comments"])
+            self.dbhelper.insert_meme_status(meme["id"], hours_elapsed, entered_hot)
+            logging.info(f"{meme['id']} is updated")
+
+            # Update if meme has newly entered hot
+            if (not is_hot) and (entered_hot):
+                self.dbhelper.update_meme_info(meme["id"], entered_hot)
+                logging.info(f"{meme['id']} has entered hot, updated info")
+
+            # Remove meme from self.update_current_ids after "update_hours" elapsed
+            if hours_elapsed >= update_hours:
+                self.update_current_ids[time].remove(meme["id"])
+        
+        self.__log_current_rate_limit()
+
+    # Removes update tasks when there is no update needed in a certain minute,
+    # After the collector stops collecting new memes
+    def __remove_update_tasks(self):
+        logging.info("Clearing unused update tasks...")
+        if schedule.get_jobs("new"):
+            return
+        for time in self.update_current_ids.keys():
+            if not self.update_current_ids[time]:
+                [job] = schedule.get_jobs(time)
+                schedule.cancel_job(job)
+                logging.info(f"Removed update task at {time}")
+
+    # Schedule new/hot collection tasks
+    def __collection_tasks(self, collect_new_hours: int):
+        schedule.every(5).minutes.do(self.collect_new_meme_data) \
+        .until(timedelta(hours = collect_new_hours)) \
+        .tag("new")
+        schedule.every(5).minutes.do(self.collect_current_hot_meme_ids) \
+        .tag("hot")
+
+    # Schedule update tasks
+    def __update_tasks(self, update_hours: int, failsafe: bool = False):
+        for time in self.update_current_ids.keys():
+            schedule.every().hour.at(time).do(
+                self.collect_existing_memes_data, time, update_hours, failsafe
+            ) \
+            .tag("update", time)
+    
+    # Schedule update task removal task
+    def __remove_tasks(self):
+        schedule.every().hour.do(
+            self.__remove_update_tasks
+        )
+
+    # Used for logging after a request
     def __log_current_rate_limit(self):
         rates = self.scraper.reddit.auth.limits
-        reset_time = datetime.datetime.fromtimestamp(rates["reset_timestamp"])
-        logging.info(f"""Rate Limit: Used: {rates["used"]} Remaining: {rates["remaining"]} Next Reset: {reset_time.strftime("%Y-%m-%d, %H:%M:%S")}""")
+        if rates['reset_timestamp'] is None:
+            logging.warning("Unable to get current rate limits.")
+            return
+        reset_time = datetime.fromtimestamp(rates['reset_timestamp'])
+        logging.info(f"Rate Limit: Used: {rates['used']} Remaining: {rates['remaining']} Next Reset: {reset_time.strftime('%Y-%m-%d, %H:%M:%S')}")
 
-    def run(self):
-        self.__scheduler()
+    # Configure and run
+    def run(self, collect_new_hours: int, update_hours: int, failsafe: bool = False):
+        self.__collection_tasks(collect_new_hours)
         schedule.run_all()
-        while True:
+        self.__update_tasks(update_hours, failsafe)
+        self.__remove_tasks()
+
+        # Stops when there is nothing to update
+        while schedule.get_jobs("update"):
             schedule.run_pending()
-            time.sleep(1)
+        logging.info("Collection finished")
